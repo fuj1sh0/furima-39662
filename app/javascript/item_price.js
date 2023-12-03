b0e734aa334f7ba854b534905fa6ce813d33b02a
fuj1sh0 <syo0930@gmail.com>
2023-12-03 20:04:52 +0900

    商品出品機能2

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,8 +1,10 @@
-const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-  const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = (Math.floor( inputValue * 0.1 ));
-  const profitValue = document.getElementById("profit");
-  profitValue.innerHTML = (Math.floor(inputValue - Math.floor(inputValue * 0.1)));
+window.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = (Math.floor( inputValue * 0.1 ));
+    const profitValue = document.getElementById("profit");
+    profitValue.innerHTML = (Math.floor(inputValue - Math.floor(inputValue * 0.1)));
+  });
 });
