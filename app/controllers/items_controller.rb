bd1c1abcf7c901028fca89f0ab686ddcdd230ea4
fuj1sh0 <syo0930@gmail.com>
2023-12-10 04:14:50 +0900

    商品購入機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :move_to_root_path, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -54,5 +55,11 @@ class ItemsController < ApplicationController
     return if current_user == @item.user
 
     redirect_to action: :index
+  end
+
+  def move_to_root_path
+    return unless @item.user_id != current_user.id || !@item.order.nil?
+
+    redirect_to root_path
   end
 end
